59550a095f4d246e85360f3f07343f76c6bca9f0
Vitalii-Krat <kratvetal@ukr.net>
2022-01-30 22:00:27 +0200

    add_folders/files

---
+++ src/componets/App/App.jsx
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+// class App extends Component {
+//   state = {
+//     images: null,
+//     page: 1,
+//     error: '',
+//     query: '',
+//   };
+
+// }
+
+// export default App;
